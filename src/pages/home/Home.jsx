57199e2070ddccbd3ce74ffec431732efe1ede2d
Sourov <sourovdas730@gmail.com>
2024-06-26 22:42:50 +0600

    set up context

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Banner from './banner/Banner';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const Home = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user);
     return (
         <div>
             {/* <h1>Home Page</h1> */}
